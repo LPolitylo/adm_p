fd81636e10125b44db24b9cdbade21eb5bcf00fe
LPolitylo <ljupol7@gmail.com>
2018-05-15 00:25:28 +0300

    add session variable

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,2 +1,7 @@
 class ApplicationController < ActionController::Base
+
+	def after_sign_in_path_for(resource_or_scope)
+		session[:allowed_actions] ||= current_user.roles.map(&:acls).flatten.map { |acl| "#{acl.class_name}::#{acl.method_name}" }
+		super
+	end
 end
