689544fd68363f9460499d5326bcb60c3f78de18
LuPol <ljupol7@gmail.com>
2018-05-25 12:01:05 +0300

    restructurized

---
+++ app/lib/sinatra/controllers/my_sin_app.rb
@@ -0,0 +1,49 @@
+class MySinApp < Sinatra::Base
+  get('/styles.css'){ scss :styles }
+  set :public_folder, "#{Rails.root}/app/lib/sinatra/public"
+  set :views, "#{Rails.root}/app/lib/sinatra/views"
+  
+  helpers AuthHelpers
+
+  before do
+    set_title
+  end
+
+  def css(*stylesheets)
+    stylesheets.map do |stylesheet|
+      "<link href=\"/#{stylesheet}.css\" media=\"screen, projection\" rel=\"stylesheet\" />"
+    end.join
+  end
+
+  def current?(path='/')
+    (request.path==path || request.path==path+'/') ? "current" : nil
+  end
+
+  def set_title
+    @title ||= "Songs By Sinatra"
+  end  
+
+
+  get '/' do
+    slim :home
+  end
+
+  get '/about' do
+    @title = "All About This Website"
+    slim :about
+  end
+
+  get '/logout' do
+    log_out!
+    redirect to('/')
+  end
+
+  not_found do
+    slim :not_found
+  end
+
+  get '/fake-error' do
+    status 500
+    "There’s nothing wrong, really :P"
+  end
+end
---
+++ app/lib/sinatra/helpers/song_helpers.rb
@@ -0,0 +1,13 @@
+module SongHelpers
+  def find_songs
+    @songs = Song.all
+  end
+
+  def find_song
+    Song.where(id: params[:id]).take!
+  end
+
+  def create_song
+    @song = Song.create(params[:song])
+  end
+end
